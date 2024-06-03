47df92a783c24f34741c0d025e0c6b83f4d38733
Ant00000ny <Ant00000ny@icloud.com>
2024-06-04 00:14:21 +0800

    添加统计

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import {Analytics} from "@vercel/analytics/react"
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "学马仕计算器",
-  description: "学马仕计算器",
+    title: "学马仕计算器",
+    description: "学马仕计算器",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        {children}
+        <Analytics/>
+        </body>
+        </html>
+    );
 }
